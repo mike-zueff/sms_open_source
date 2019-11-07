5edac5b0c11b802b2381e3100a102e7971b93904
Mike Zueff <mike.zueff@gmail.com>
2019-11-07 08:36:50 +0300

    Implement app stub

---
+++ lib.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$vk_api_client = new VK\Client\VKApiClient();
+$vk_api_token = trim(file_get_contents('private/vk_api_token'));
---
+++ sms.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+require __DIR__ . '/lib.php';
+
+/*
+$response = $vk_api_client->users()->get($vk_api_token, array(
+  'fields' => array('city', 'photo'),
+  'user_ids' => array(1, 210700286),
+));
+var_dump($response);
+*/
