63e9b62e8602977804b318da4d20b2cb5656c0e5
Mike Zueff <mike.zueff@gmail.com>
2019-11-07 22:53:08 +0300

    Restore sms.php state

--- sms.php
+++ sms.php
@@ -1,10 +1,10 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require __DIR__ . '/lib/lib.php';
 
-//sms_groups_watched_get_data();
+sms_groups_watched_fetch_data();
