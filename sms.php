9d91a76fa78ae6ef12d17ee96b409c9a032dbf6a
Mike Zueff <mike.zueff@gmail.com>
2019-11-07 08:53:27 +0300

    Improve error handling

--- sms.php
+++ sms.php
@@ -1,7 +1,10 @@
 <?php
 declare(strict_types=1);
-require __DIR__ . '/lib.php';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
+require __DIR__ . '/lib/lib.php';
 /*
 $response = $vk_api_client->users()->get($vk_api_token, array(
   'fields' => array('city', 'photo'),
