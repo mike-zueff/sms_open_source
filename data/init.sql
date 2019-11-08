126c6a1800095967a4508ad7cfdde324ec5543f0
Mike Zueff <mike.zueff@gmail.com>
2019-11-08 21:47:55 +0300

    Updated DB schema

--- data/init.sql
+++ data/init.sql
@@ -1,7 +1,19 @@
 CREATE TABLE wall_get (
+  city_id integer,
   date integer,
   from_id integer,
   owner_id integer,
+  post_id integer,
+  text text
+);
+
+CREATE TABLE wall_get_comments (
+  city_id integer,
+  comment_id integer,
+  date integer,
+  from_id integer,
+  owner_id integer,
+  parent_comment_id integer,
   post_id integer,
   text text
 );
