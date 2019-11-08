a57ea288970664677f87331e387186656426b25e
Mike Zueff <mike.zueff@gmail.com>
2019-11-08 12:02:55 +0300

    Perform some cleanup

--- lib/lib.php
+++ lib/lib.php
@@ -2,17 +2,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 const B_DEBUG_ENABLED = true;
-const I_E_WALL_GET_CONTENT_NOT_AVAILABLE = 19;
-const I_E_WALL_GET_PAGE_REMOVED_OR_BLOCKED = 18;
-const I_E_WALL_GET_THIS_PROFILE_IS_PRIVATE = 30;
-
-/* Materials older than 6 years are obsolete. */
-/* TODO */
-/* const I_MATERIAL_DATE_LIMIT = 60 * 60 * 24 * 366 * 6; */
-const I_MATERIAL_DATE_LIMIT = 60 * 60 * 24 * 7;
-
-const I_VK_API_DEFAULT_WALL_GET_COUNT = 100;
-const S_DB_PATH = 'data/sms_db.sqlite';
+const I_VK_API_WALL_GET_COUNT_DEFAULT = 100;
 
 function sms_debug($s_message) {
   if (B_DEBUG_ENABLED) {
@@ -20,64 +10,93 @@ function sms_debug($s_message) {
   }
 }
 
-function sms_groups_watched_get_data() {
-  global $a_groups_watched;
-  global $i_timestamp;
-  global $o_vk_api_client;
-  global $s_vk_api_token;
+function sms_echo($s_message) {
+  echo $s_message . PHP_EOL;
+}
+
+function sms_groups_watched_fetch() {
+  global $o_sqlite;
+
+  $a_groups_watched = file('private/groups_watched.txt', FILE_IGNORE_NEW_LINES);
 
   foreach ($a_groups_watched as $s_gw) {
     $i_offset = 0;
 
     do {
-      sms_debug('wall.get, ' . $s_gw . ', ' . $i_offset);
+      $o_result = sms_vk_api_wall_get($s_gw, $i_offset);
+      //////////////////////////////////////////////// TODO
 
-      try {
-        $o_response = $o_vk_api_client->wall()->get($s_vk_api_token, array(
-          'count' => I_VK_API_DEFAULT_WALL_GET_COUNT,
-          'offset' => $i_offset,
-          'owner_id' => $s_gw,
-        ));
+      if ($o_result != null) {
+        echo "!!!!";
+        foreach ($o_result['items'] as $o_ri) {
+          if ($o_sqlite->querySingle('SELECT * FROM wall_get WHERE post_id = ', $o_ri['id']) != null) {
+            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!" . PHP_EOL;
+            break;
+          } else {
+            $i_db_date = $o_ri['date'];
+            $i_db_from_id = $o_ri['from_id'];
+            $i_db_owner_id = $o_ri['owner_id'];
+            $i_db_post_id = $o_ri['id'];
+            $s_db_text = $o_ri['text'];
 
-      } catch (Exception $e) {
-        switch ($e->getCode()) {
-        case I_E_WALL_GET_CONTENT_NOT_AVAILABLE:
-          break;
-        case I_E_WALL_GET_PAGE_REMOVED_OR_BLOCKED:
-          break;
-        case I_E_WALL_GET_THIS_PROFILE_IS_PRIVATE:
-          break;
-        default:
-          break;
+            $o_sqlite->exec("INSERT INTO wall_get(date, from_id, owner_id, post_id, text) VALUES('$i_db_date', '$i_db_from_id', '$i_db_owner_id', '$i_db_post_id', '$s_db_text')");
+          }
         }
+      } else {
+        sms_log('error, wall.get, https://vk.com/wall-' . $s_gw . '?own=1&offset=' . $i_offset);
+        break;
       }
 
-
-      $i_offset += I_VK_API_DEFAULT_WALL_GET_COUNT;
-    } while ($o_response['items'][1]['date'] >= $i_timestamp - I_MATERIAL_DATE_LIMIT);
+      //////////////////////////////////////////////// TODO
+      //TODO while ($o_response['items'][1]['date'] >= $i_timestamp - I_MATERIAL_DATE_LIMIT);
+      $i_offset += I_VK_API_WALL_GET_COUNT_DEFAULT;
+    } while (true);
   }
 }
 
 function sms_log($s_message) {
-  echo $s_message . PHP_EOL;
+  global $r_log_file;
+
+  fwrite($r_log_file, $s_message . PHP_EOL);
+  sms_echo($s_message);
 }
 
 function sms_shutdown() {
-  sms_log('SMS stopped.');
+  global $r_log_file;
+
+  fclose($r_log_file);
+  sms_echo('SMS stopped.');
 }
 
-$a_groups_watched = file('private/groups_watched', FILE_IGNORE_NEW_LINES);
-$o_settlements = json_decode(file_get_contents('data/vor_obl_settlements.json'));
-$o_sqlite = null;
+function sms_vk_api_wall_get($i_owner_id, $i_offset) {
+  global $o_vk_api_client;
+  global $s_vk_api_token;
+
+  sms_debug('wall.get, ' . $i_owner_id . ', ' . $i_offset);
+
+  try {
+    $o_response = $o_vk_api_client->wall()->get($s_vk_api_token, array(
+      'count' => I_VK_API_WALL_GET_COUNT_DEFAULT,
+      'offset' => $i_offset,
+      'owner_id' => $i_owner_id,
+    ));
+
+    return $o_response;
+  } catch (Exception $e) {
+    return null;
+  }
+}
+
+$o_sqlite = new SQLite3('data/sms_db.sqlite');
 $o_vk_api_client = new VK\Client\VKApiClient();
-$s_vk_api_token = trim(file_get_contents('private/vk_api_token'));
+$r_log_file = fopen('data/log.txt', 'w');
+$s_vk_api_token = trim(file_get_contents('private/vk_api_token.txt'));
 
 date_default_timezone_set('Europe/Moscow');
 $i_timestamp = time();
-
 register_shutdown_function('sms_shutdown');
-sms_log('SMS started.');
-
-$o_sqlite = new SQLite3(S_DB_PATH);
-$o_result = $o_sqlite->query('SELECT * FROM wall_get');
-var_dump($o_result);
+sms_echo('SMS started.');
+//TODO $o_settlements = json_decode(file_get_contents('data/vor_obl_settlements.json'));
+//TODO Materials older than 6 years are obsolete.
+//TODO const I_MATERIAL_DATE_LIMIT = 60 * 60 * 24 * 366 * 6;
+//TODO const I_MATERIAL_DATE_LIMIT = 60 * 60 * 24 * 2;
