55fcb9c8455ccd048ffc40988347e0a77d51b367
Mike Zueff <mike.zueff@gmail.com>
2019-11-12 14:11:55 +0300

    Update DB schema

--- data/init.sql
+++ data/init.sql
@@ -1,21 +1,21 @@
 CREATE TABLE wall_get (
   attachments text,
-  city_id integer,
   date integer,
   from_id integer,
   owner_id integer,
   post_id integer,
+  settlement_id integer,
   text text
 );
 
 CREATE TABLE wall_getcomments (
   attachments text,
-  city_id integer,
   comment_id integer,
   date integer,
   from_id integer,
   owner_id integer,
   parent_comment_id integer,
   post_id integer,
+  settlement_id integer,
   text text
 );
