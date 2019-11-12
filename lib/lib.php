69ed787fd749f8b0515d3e5771ff6d2f1157de56
Mike Zueff <mike.zueff@gmail.com>
2019-11-12 11:03:52 +0300

    Update library source code

--- lib/lib.php
+++ lib/lib.php
@@ -3,9 +3,114 @@ require __DIR__ . '/../vendor/autoload.php';
 
 const B_DEBUG_ENABLED = true;
 /* TODO Fix all limits. */
-const I_DATE_LIMIT_WALL_GET_GROUP = 60 * 60 * 24 * 1;
+const I_DATE_LIMIT_WALL_GETCOMMENTS_GROUP = 60 * 60 * 24 * 1;
+const I_DATE_LIMIT_WALL_GET_GROUP = 60 * 60 * 1;
 const I_DATE_LIMIT_WALL_GET_USER = 60 * 60 * 24 * 26;
-const I_VK_API_WALL_GET_COUNT_DEFAULT = 100;
+const I_NULL_VALUE = -1;
+const I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT = 100;
+const I_VK_API_WALL_GET_COUNT_DEFAULT = 3;
+
+function sms_db_posts_fetch_comments() {
+  global $i_timestamp;
+  global $o_sqlite;
+
+  $o_db_data_posts = $o_sqlite->query('SELECT * FROM wall_get');
+
+  while ($a_i = $o_db_data_posts->fetchArray()) {
+    $i_offset = 0;
+
+    if ($a_i['date'] < $i_timestamp - I_DATE_LIMIT_WALL_GETCOMMENTS_GROUP) {
+      continue;
+    }
+
+    do {
+      $b_need_for_break = false;
+      $o_result = sms_vk_api_wall_getcomments($a_i['owner_id'], $a_i['post_id'], $i_offset, I_NULL_VALUE);
+
+      if ($o_result != null) {
+        if (count($o_result['items']) != 0) {
+          $b_need_for_break = true;
+        }
+
+        foreach ($o_result['items'] as $o_ri) {
+          if (!array_key_exists('deleted', $o_ri)) {
+            $i_db_comment_id = $o_ri['id'];
+            $i_db_date = $o_ri['date'];
+            $i_db_from_id = $o_ri['from_id'];
+            $i_db_owner_id = $o_ri['owner_id'];
+            $i_db_parent_comment_id = I_NULL_VALUE;
+            $i_db_post_id = $o_ri['post_id'];
+            $i_offset_nested = 0;
+            $s_db_text = $o_ri['text'];
+
+            if (array_key_exists('attachments', $o_ri)) {
+              $s_db_attachments = serialize($o_ri['attachments']);
+            } else {
+              $s_db_attachments = '';
+            }
+
+            if (array_key_exists('city', $o_ri)) {
+              $i_db_city_id = $o_ri['city']['id'];
+            } else {
+              $i_db_city_id = I_NULL_VALUE;
+            }
+
+            $o_sqlite->exec("INSERT INTO wall_getcomments(attachments, city_id, comment_id, date, from_id, owner_id, parent_comment_id, post_id, text) VALUES('$s_db_attachments', $i_db_city_id, $i_db_comment_id, $i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_parent_comment_id, $i_db_post_id, '$s_db_text')");
+
+            if ($o_ri['thread']['count'] > 0) {
+              do {
+                $b_need_for_break_nested = false;
+                $o_result_nested = sms_vk_api_wall_getcomments($a_i['owner_id'], $a_i['post_id'], $i_offset_nested, $i_db_comment_id);
+
+                if ($o_result_nested != null) {
+                  if (count($o_result_nested['items']) != 0) {
+                    $b_need_for_break_nested = true;
+                  }
+
+                  foreach ($o_result_nested['items'] as $o_rin) {
+                    if (!array_key_exists('deleted', $o_rin)) {
+                      $i_db_comment_id_nested = $o_rin['id'];
+                      $i_db_date_nested = $o_rin['date'];
+                      $i_db_from_id_nested = $o_rin['from_id'];
+                      $i_db_owner_id_nested = $o_rin['owner_id'];
+                      $i_db_parent_comment_id_nested = $i_db_comment_id;
+                      $i_db_post_id_nested = $o_rin['post_id'];
+                      $s_db_text_nested = $o_rin['text'];
+
+                      if (array_key_exists('attachments', $o_rin)) {
+                        $s_db_attachments_nested = serialize($o_rin['attachments']);
+                      } else {
+                        $s_db_attachments_nested = '';
+                      }
+
+                      if (array_key_exists('city', $o_rin)) {
+                        $i_db_city_id_nested = $o_rin['city']['id'];
+                      } else {
+                        $i_db_city_id_nested = I_NULL_VALUE;
+                      }
+
+                      $o_sqlite->exec("INSERT INTO wall_getcomments(attachments, city_id, comment_id, date, from_id, owner_id, parent_comment_id, post_id, text) VALUES('$s_db_attachments_nested', $i_db_city_id_nested, $i_db_comment_id_nested, $i_db_date_nested, $i_db_from_id_nested, $i_db_owner_id_nested, $i_db_parent_comment_id_nested, $i_db_post_id_nested, '$s_db_text_nested')");
+                    }
+                  }
+                } else {
+                  sms_log('error, wall.getcomments, nested, https://vk.com/wall' . $i_db_owner_id . '_' . $i_db_post_id . '?reply=' . $i_db_comment_id);
+                  break;
+                }
+
+                $i_offset_nested += I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT;
+              } while (!$b_need_for_break_nested);
+            }
+          }
+        }
+      } else {
+        sms_log('error, wall.getcomments, https://vk.com/wall' . $a_i['owner_id'] . '_' . $a_i['post_id'] . '?reply=' . $a_i['comment_id']);
+        break;
+      }
+
+      $i_offset += I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT;
+    } while (!$b_need_for_break);
+  }
+}
 
 function sms_debug($s_message) {
   if (B_DEBUG_ENABLED) {
@@ -40,6 +145,8 @@ function sms_vk_api_wall_get($i_owner_id, $i_offset) {
   try {
     $o_response = $o_vk_api_client->wall()->get($s_vk_api_token, array(
       'count' => I_VK_API_WALL_GET_COUNT_DEFAULT,
+      'extended' => 1,
+      'fields' => 'city',
       'offset' => $i_offset,
       'owner_id' => $i_owner_id,
     ));
@@ -50,18 +157,48 @@ function sms_vk_api_wall_get($i_owner_id, $i_offset) {
   }
 }
 
-function sms_watched_groups_wall_get() {
+function sms_vk_api_wall_getcomments($i_owner_id, $i_post_id, $i_offset, $i_comment_id) {
+  global $o_vk_api_client;
+  global $s_vk_api_token;
+
+  sms_debug('wall.getcomments, ' . $i_owner_id . ', ' . $i_post_id . ', ' . $i_offset . ', ' . $i_comment_id);
+
+  $a_getcomments = [
+    'count' => I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT,
+    'extended' => 1,
+    'fields' => 'city',
+    'offset' => $i_offset,
+    'owner_id' => $i_owner_id,
+    'post_id' => $i_post_id,
+    'preview_length' => 0,
+    'sort' => 'asc',
+  ];
+
+  if ($i_comment_id != I_NULL_VALUE) {
+    $a_getcomments['comment_id'] = $i_comment_id;
+  }
+
+  try {
+    $o_response = $o_vk_api_client->wall()->getComments($s_vk_api_token, $a_getcomments);
+
+    return $o_response;
+  } catch (Exception $e) {
+    return null;
+  }
+}
+
+function sms_watched_owners_wall_get() {
   global $i_timestamp;
   global $o_sqlite;
 
-  $a_watched_groups = file('private/watched_groups.txt', FILE_IGNORE_NEW_LINES);
+  $a_watched_owners = file('private/watched_owners.txt', FILE_IGNORE_NEW_LINES);
 
-  foreach ($a_watched_groups as $s_wg) {
+  foreach ($a_watched_owners as $s_wo) {
     $i_offset = 0;
 
     do {
       $b_need_for_break = false;
-      $o_result = sms_vk_api_wall_get($s_wg, $i_offset);
+      $o_result = sms_vk_api_wall_get($s_wo, $i_offset);
 
       if ($o_result != null) {
         foreach ($o_result['items'] as $o_ri) {
@@ -81,11 +218,23 @@ function sms_watched_groups_wall_get() {
             $i_db_owner_id = $o_ri['owner_id'];
             $s_db_text = $o_ri['text'];
 
-            $o_sqlite->exec("INSERT INTO wall_get(date, from_id, owner_id, post_id, text) VALUES($i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_post_id, '$s_db_text')");
+            if (array_key_exists('attachments', $o_ri)) {
+              $s_db_attachments = serialize($o_ri['attachments']);
+            } else {
+              $s_db_attachments = '';
+            }
+
+            if (array_key_exists('city', $o_ri)) {
+              $i_db_city_id = $o_ri['city']['id'];
+            } else {
+              $i_db_city_id = I_NULL_VALUE;
+            }
+
+            $o_sqlite->exec("INSERT INTO wall_get(attachments, city_id, date, from_id, owner_id, post_id, text) VALUES('$s_db_attachments', $i_db_city_id, $i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_post_id, '$s_db_text')");
           }
         }
       } else {
-        sms_log('error, wall.get, https://vk.com/wall-' . $s_wg . '?own=1&offset=' . $i_offset);
+        sms_log('error, wall.get, https://vk.com/wall-' . $s_wo . '?own=1&offset=' . $i_offset);
         break;
       }
 
