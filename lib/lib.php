78eaf821085eb0ef90a5858f0fe0757af44b6cc2
Mike Zueff <mike.zueff@gmail.com>
2019-11-12 14:12:38 +0300

    Implement settlements checker

--- lib/lib.php
+++ lib/lib.php
@@ -3,12 +3,14 @@ require __DIR__ . '/../vendor/autoload.php';
 
 const B_DEBUG_ENABLED = true;
 /* TODO Fix all limits. */
-const I_DATE_LIMIT_WALL_GETCOMMENTS_GROUP = 60 * 60 * 24 * 1;
-const I_DATE_LIMIT_WALL_GET_GROUP = 60 * 60 * 1;
-const I_DATE_LIMIT_WALL_GET_USER = 60 * 60 * 24 * 26;
+const I_DATE_LIMIT_WALL_GETCOMMENTS = 60 * 60 * 4 * 1;
+const I_DATE_LIMIT_WALL_GET = 60 * 60 * 4 * 1;
 const I_NULL_VALUE = -1;
-const I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT = 100;
-const I_VK_API_WALL_GET_COUNT_DEFAULT = 3;
+const I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT = 5; //100 TODO
+const I_VK_API_WALL_GET_COUNT_DEFAULT = 5; //100 TODO
+
+function sms_db_analyze_data() {
+}
 
 function sms_db_posts_fetch_comments() {
   global $i_timestamp;
@@ -19,7 +21,7 @@ function sms_db_posts_fetch_comments() {
   while ($a_i = $o_db_data_posts->fetchArray()) {
     $i_offset = 0;
 
-    if ($a_i['date'] < $i_timestamp - I_DATE_LIMIT_WALL_GETCOMMENTS_GROUP) {
+    if ($a_i['date'] < $i_timestamp - I_DATE_LIMIT_WALL_GETCOMMENTS) {
       continue;
     }
 
@@ -49,9 +51,9 @@ function sms_db_posts_fetch_comments() {
               $s_db_attachments = '';
             }
 
-            $i_db_city_id = sms_user_fetch_city($i_db_from_id);
+            $i_db_settlement_id = sms_user_fetch_settlement_id($i_db_from_id);
 
-            $o_sqlite->exec("INSERT INTO wall_getcomments(attachments, city_id, comment_id, date, from_id, owner_id, parent_comment_id, post_id, text) VALUES('$s_db_attachments', $i_db_city_id, $i_db_comment_id, $i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_parent_comment_id, $i_db_post_id, '$s_db_text')");
+            $o_sqlite->exec("INSERT INTO wall_getcomments(attachments, settlement_id, comment_id, date, from_id, owner_id, parent_comment_id, post_id, text) VALUES('$s_db_attachments', $i_db_settlement_id, $i_db_comment_id, $i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_parent_comment_id, $i_db_post_id, '$s_db_text')");
 
             if ($o_ri['thread']['count'] > 0) {
               do {
@@ -79,13 +81,15 @@ function sms_db_posts_fetch_comments() {
                         $s_db_attachments_nested = '';
                       }
 
-                      $i_db_city_id_nested = sms_user_fetch_city($i_db_from_id_nested);
+                      $i_db_settlement_id_nested = sms_user_fetch_settlement_id($i_db_from_id_nested);
 
-                      $o_sqlite->exec("INSERT INTO wall_getcomments(attachments, city_id, comment_id, date, from_id, owner_id, parent_comment_id, post_id, text) VALUES('$s_db_attachments_nested', $i_db_city_id_nested, $i_db_comment_id_nested, $i_db_date_nested, $i_db_from_id_nested, $i_db_owner_id_nested, $i_db_parent_comment_id_nested, $i_db_post_id_nested, '$s_db_text_nested')");
+                      if (sms_settlement_check($i_db_settlement_id_nested)) {
+                        $o_sqlite->exec("INSERT INTO wall_getcomments(attachments, settlement_id, comment_id, date, from_id, owner_id, parent_comment_id, post_id, text) VALUES('$s_db_attachments_nested', $i_db_settlement_id_nested, $i_db_comment_id_nested, $i_db_date_nested, $i_db_from_id_nested, $i_db_owner_id_nested, $i_db_parent_comment_id_nested, $i_db_post_id_nested, '$s_db_text_nested')");
+                      }
                     }
                   }
                 } else {
-                  sms_log('error, wall.getcomments, nested, https://vk.com/wall' . $i_db_owner_id . '_' . $i_db_post_id . '?reply=' . $i_db_comment_id);
+                  sms_echo('error, wall.getcomments, nested, https://vk.com/wall' . $i_db_owner_id . '_' . $i_db_post_id . '?reply=' . $i_db_comment_id);
                   break;
                 }
 
@@ -95,7 +99,7 @@ function sms_db_posts_fetch_comments() {
           }
         }
       } else {
-        sms_log('error, wall.getcomments, https://vk.com/wall' . $a_i['owner_id'] . '_' . $a_i['post_id'] . '?reply=' . $a_i['comment_id']);
+        sms_echo('error, wall.getcomments, https://vk.com/wall' . $a_i['owner_id'] . '_' . $a_i['post_id']);
         break;
       }
 
@@ -121,6 +125,22 @@ function sms_log($s_message) {
   sms_echo($s_message);
 }
 
+function sms_settlement_check($i_settlement_id) {
+  global $a_settlements;
+
+  if($i_settlement_id == I_NULL_VALUE) {
+    return false;
+  }
+
+  foreach ($a_settlements['items'] as $a_si) {
+    if ($a_si['id'] == $i_settlement_id) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function sms_shutdown() {
   global $r_log_file;
 
@@ -128,13 +148,13 @@ function sms_shutdown() {
   sms_echo('SMS stopped.');
 }
 
-function sms_user_fetch_city($i_user_id) {
+function sms_user_fetch_settlement_id($i_user_id) {
   $o_result = sms_vk_api_user_get($i_user_id, 'city');
 
   if ($o_result != null && array_key_exists('city', $o_result[0])) {
     return $o_result[0]['city']['id'];
   } else {
-    return -1;
+    return I_NULL_VALUE;
   }
 }
 
@@ -169,8 +189,6 @@ function sms_vk_api_wall_get($i_owner_id, $i_offset) {
   try {
     $o_response = $o_vk_api_client->wall()->get($s_vk_api_token, array(
       'count' => I_VK_API_WALL_GET_COUNT_DEFAULT,
-      'extended' => 1,
-      'fields' => 'city',
       'offset' => $i_offset,
       'owner_id' => $i_owner_id,
     ));
@@ -189,8 +207,6 @@ function sms_vk_api_wall_getcomments($i_owner_id, $i_post_id, $i_offset, $i_comm
 
   $a_getcomments = [
     'count' => I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT,
-    'extended' => 1,
-    'fields' => 'city',
     'offset' => $i_offset,
     'owner_id' => $i_owner_id,
     'post_id' => $i_post_id,
@@ -226,9 +242,10 @@ function sms_watched_owners_wall_get() {
 
       if ($o_result != null) {
         foreach ($o_result['items'] as $o_ri) {
+          $i_db_owner_id = $o_ri['owner_id'];
           $i_db_post_id = $o_ri['id'];
 
-          if ($o_sqlite->querySingle("SELECT * FROM wall_get WHERE post_id = $i_db_post_id") != null) {
+          if ($o_sqlite->querySingle("SELECT * FROM wall_get WHERE owner_id = $i_db_owner_id AND post_id = $i_db_post_id") != null) {
             if (!array_key_exists('is_pinned', $o_ri)) {
               $b_need_for_break = true;
 
@@ -239,7 +256,6 @@ function sms_watched_owners_wall_get() {
           } else {
             $i_db_date = $o_ri['date'];
             $i_db_from_id = $o_ri['from_id'];
-            $i_db_owner_id = $o_ri['owner_id'];
             $s_db_text = $o_ri['text'];
 
             if (array_key_exists('attachments', $o_ri)) {
@@ -248,25 +264,26 @@ function sms_watched_owners_wall_get() {
               $s_db_attachments = '';
             }
 
-            $i_db_city_id = sms_user_fetch_city($i_db_from_id);
+            $i_db_settlement_id = sms_user_fetch_settlement_id($i_db_from_id);
 
-            $o_sqlite->exec("INSERT INTO wall_get(attachments, city_id, date, from_id, owner_id, post_id, text) VALUES('$s_db_attachments', $i_db_city_id, $i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_post_id, '$s_db_text')");
+            $o_sqlite->exec("INSERT INTO wall_get(attachments, settlement_id, date, from_id, owner_id, post_id, text) VALUES('$s_db_attachments', $i_db_settlement_id, $i_db_date, $i_db_from_id, $i_db_owner_id, $i_db_post_id, '$s_db_text')");
           }
         }
       } else {
-        sms_log('error, wall.get, https://vk.com/wall-' . $s_wo . '?own=1&offset=' . $i_offset);
+        sms_echo('error, wall.get, https://vk.com/wall' . $s_wo . '?own=1&offset=' . $i_offset);
         break;
       }
 
       $i_offset += I_VK_API_WALL_GET_COUNT_DEFAULT;
 
-      if ($b_need_for_break || count($o_result['items']) == 1 || $i_timestamp > $o_result['items'][1]['date'] + I_DATE_LIMIT_WALL_GET_GROUP) {
+      if ($b_need_for_break || count($o_result['items']) == 1 || $i_timestamp > $o_result['items'][1]['date'] + I_DATE_LIMIT_WALL_GET) {
         break;
       }
     } while (true);
   }
 }
 
+$a_settlements = json_decode(file_get_contents('data/vor_obl_settlements.json'), true);
 $o_sqlite = new SQLite3('data/sms_db.sqlite');
 $o_vk_api_client = new VK\Client\VKApiClient();
 $r_log_file = fopen('data/log.txt', 'w');
@@ -276,4 +293,3 @@ date_default_timezone_set('Europe/Moscow');
 $i_timestamp = time();
 register_shutdown_function('sms_shutdown');
 sms_echo('SMS started.');
-//TODO $o_settlements = json_decode(file_get_contents('data/vor_obl_settlements.json'));
