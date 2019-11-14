756584cce0411ef8b7e0e3ed83159083119a9bf9
Mike Zueff <mike.zueff@gmail.com>
2019-11-14 22:56:42 +0300

    Update time limits.

--- lib/lib.php
+++ lib/lib.php
@@ -2,8 +2,8 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 const B_DEBUG_ENABLED = true;
-const I_DATE_LIMIT_WALL_GET = 60 * 60 * 24 * 4;
-const I_DATE_LIMIT_WALL_GETCOMMENTS = 60 * 60 * 24 * 4;
+const I_DATE_LIMIT_WALL_GET = 60 * 60 * 24 * 2;
+const I_DATE_LIMIT_WALL_GETCOMMENTS = 60 * 60 * 24 * 2;
 const I_NULL_VALUE = -1;
 const I_SLEEP_TIME = 1;
 const I_VK_API_WALL_GETCOMMENTS_COUNT_DEFAULT = 100;
