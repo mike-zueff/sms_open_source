52b9cf1569deead4702f58d32a61be7a7cd8fbd8
Mike Zueff <mike.zueff@gmail.com>
2019-11-21 19:31:41 +0300

    Restore general routine

--- sms.php
+++ sms.php
@@ -10,9 +10,9 @@ require __DIR__ . '/lib/lib.php';
 $a_getopt = getopt('a');
 
 if (!array_key_exists('a', $a_getopt)) {
-  sms_watched_owners_wall_get();
   sms_db_delete_obsolete_posts();
   sms_db_delete_obsolete_comments();
+  sms_watched_owners_wall_get();
   sms_db_posts_fetch_comments();
 }
 
