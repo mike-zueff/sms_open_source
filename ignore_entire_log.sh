6e0a5915ddd096c18eb15e28a2745cd8713a22e0
Mike Zueff <mike.zueff@gmail.com>
2020-01-05 15:24:46 +0300

    Update script

--- ignore_entire_log.sh
+++ ignore_entire_log.sh
@@ -4,5 +4,6 @@ LOG_FILE=data/log.txt
 
 grep "|post|" ${LOG_FILE}
 grep "|comment|" ${LOG_FILE}
+grep "|nested_comment|" ${LOG_FILE}
 grep "|photo_comment|" ${LOG_FILE}
 grep "|video_comment|" ${LOG_FILE}
